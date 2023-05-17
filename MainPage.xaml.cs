5921567610031e014ee800986b4090022d64244d
HAMED <130986160+HMDMHMDI@users.noreply.github.com>
2023-05-17 13:14:45 +0330

    second edit

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,25 +1,59 @@
-﻿namespace ColorMauiApp;
+﻿using System.Diagnostics;
+using CommunityToolkit.Maui.Alerts;
+
+namespace ColorMauiApp;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    bool isRandom;
+    string HexValue; 
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    void Slider_ValueChanged(System.Object sender, Microsoft.Maui.Controls.ValueChangedEventArgs e)
+    {
+        if (!isRandom)
+        {
+            var red = SldRed.Value;
+            var green = SldGreen.Value;
+            var blue = SldBlue.Value;
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+            Color color = Color.FromRgb(red, green, blue);
+            SetColor(color);
+        }
+        
+    }
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+    private void SetColor(Color color)
+    {
+        Debug.WriteLine(color.ToString());
+        btnRandom.BackgroundColor = color;
+        Container.BackgroundColor = color;
+        HexValue = color.ToHex();
+        lblHex.Text = HexValue;
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    void btnRandom_Clicked(System.Object sender, System.EventArgs e)
+    {
+        isRandom = true;
+        var random = new Random();
+        var color = Color.FromRgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+        SetColor(color);
+        SldRed.Value = color.Red;
+        SldGreen.Value = color.Green;
+        SldBlue.Value = color.Blue;
+        isRandom = false;
+    }
+
+    async void ImageButton_Clicked(System.Object sender, System.EventArgs e)
+    {
+        await Clipboard.SetTextAsync(HexValue);
+        var toast = Toast.Make("Color Copied" , CommunityToolkit.Maui.Core.ToastDuration.Short,12);
+        await toast.Show();
+         
+    }
 }
 
 
